a03491295ec71bb9a671ada8ec41dace96b1e1e9
MJ Rossetti (@s2t2) <s2t2mail@gmail.com>
2015-02-21 14:15:16 -0800

    installing and configuring node

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("List all the govs!")
+
+var mongoose = require('mongoose')
+
+//mongoose.connect('mongodb://localhost/test')
+
+//var Cat = mongoose.model('Cat', { name: String })
+//
+//var kitty = new Cat({ name: 'Zildjian' })
+//kitty.save(function (err) {
+//  if (err) // ...
+//  console.log('meow')
+//})
+
